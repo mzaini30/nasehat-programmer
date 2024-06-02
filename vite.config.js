a03f38cf90b287ed70a4ff8365fb09bf4066c287
mzaini30 <muhzaini30@gmail.com>
2024-06-02 19:03:53 +0800

    chore: upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { preprocess } from "svelte/compiler";
+import { mdsvex } from "mdsvex";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [
+    svelte({
+      extensions: [".svelte", ".md"],
+      preprocess: mdsvex({
+        extensions: [".md"],
+      }),
+    }),
+  ],
+});
